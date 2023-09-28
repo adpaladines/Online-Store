dbbf75962ce476173a91e865bbbb7c65154d2e9b
Andres D. Paladines <andrespaladines@hotmail.com>
2023-09-28 11:26:15 -0400

    fix: TabBar now not transparent.

--- TheSoundsStoreCoreData/TheSoundsStoreCoreData/View/SearchScreen/SearchScreeen.swift
+++ TheSoundsStoreCoreData/TheSoundsStoreCoreData/View/SearchScreen/SearchScreeen.swift
@@ -13,9 +13,9 @@ struct SearchScreeen: View {
     
     @EnvironmentObject var viewModel: ProductsListViewModel
     
-//    @FetchRequest(entity: ProductEntity.entity(), sortDescriptors: [])
-//    var results: FetchedResults<ProductEntity>
-//    var request: NSFetchRequest<ProductEntity> = ProductEntity.fetchRequest()
+    //    @FetchRequest(entity: ProductEntity.entity(), sortDescriptors: [])
+    //    var results: FetchedResults<ProductEntity>
+    //    var request: NSFetchRequest<ProductEntity> = ProductEntity.fetchRequest()
     
     @State var searchText: String = ""
     @State var hasError1: Bool = false
@@ -51,7 +51,7 @@ struct SearchScreeen: View {
                     .padding([.bottom], 20)
             }
         }
-    }    
+    }
     
     func getUserData() async {
         await viewModel.getUserDataFrom(urlString: ApiManager.userInfo())
@@ -88,86 +88,90 @@ struct SearchScreeen: View {
         }
         
         productList = await viewModel.getProductsListFromGenericDB()
-
+        
     }
     
-//    func getDBDirectory() {
-//        guard let url = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask).first
-//        else {
-//            return
-//        }
-//        let sqlitepath = url.appendingPathComponent("TheSoundsStoreCoreData")
-//        print(sqlitepath)
-//    }
+    //    func getDBDirectory() {
+    //        guard let url = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask).first
+    //        else {
+    //            return
+    //        }
+    //        let sqlitepath = url.appendingPathComponent("TheSoundsStoreCoreData")
+    //        print(sqlitepath)
+    //    }
     
     var body: some View {
         NavigationStack {
-            SearchBar(searchText: $searchText)
-                .padding()
+            VStack {
+                SearchBar(searchText: $searchText)
+                    .padding()
                 
-            FiltersBar(categories: viewModel.categoriesList)
-                .task {
-                    await getCategories()
-                }
-
-            ScrollView {
-                generateProductsListFromDB()
+                FiltersBar(categories: viewModel.categoriesList)
                     .task {
-                        await getProducts()
+                        await getCategories()
                     }
-//                    .refreshable {
-//                        await refreshProduct()
-//                        await getCategories()
-//                    }
                 
-                SectionHeader(leftText: "Viewed Items", rightText: "See all")
-                if viewModel.recentsList.count > 0 {
-                    ScrollView(.horizontal) {
-                        LazyHGrid(rows: [GridItem(.flexible())]) {
-                            ForEach(viewModel.recentsList) { item in
-                                ProductCellWithButton(item: item)
-                                
-                            }.padding([.horizontal], 4)
+                ScrollView {
+                    generateProductsListFromDB()
+                        .task {
+                            await getProducts()
+                        }
+                    //                    .refreshable {
+                    //                        await refreshProduct()
+                    //                        await getCategories()
+                    //                    }
+                    
+                    SectionHeader(leftText: "Viewed Items", rightText: "See all")
+                    if viewModel.recentsList.count > 0 {
+                        ScrollView(.horizontal) {
+                            LazyHGrid(rows: [GridItem(.flexible())]) {
+                                ForEach(viewModel.recentsList) { item in
+                                    ProductCellWithButton(item: item)
+                                    
+                                }.padding([.horizontal], 4)
+                            }
+                        }
+                        .padding([.bottom])
+                    } else {
+                        VStack {
+                            Image("no-data-found")
+                                .resizable()
+                                .frame(width: 128, height: 100)
+                                .cornerRadius(25, corners: .topLeft)
+                                .cornerRadius(25, corners: .bottomRight)
+                            Text("No items viewed")
+                                .fontWeight(.semibold)
+                                .foregroundColor(.gray)
+                                .padding([.bottom], 20)
                         }
                     }
-                    .padding([.bottom])
-                } else {
-                    VStack {
-                        Image("no-data-found")
-                            .resizable()
-                            .frame(width: 128, height: 100)
-                            .cornerRadius(25, corners: .topLeft)
-                            .cornerRadius(25, corners: .bottomRight)
-                        Text("No items viewed")
-                            .fontWeight(.semibold)
-                            .foregroundColor(.gray)
-                            .padding([.bottom], 20)
+                }
+                .redacted(reason: loading ? .placeholder: [])
+                .onAppear {
+                    UITabBar.showTabBar(animated: true)
+                    
+                    guard let url = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask).first
+                    else {
+                        return
+                    }
+                    let sqlitepath = url.appendingPathComponent("TheSoundsStoreCoreData")
+                    print(sqlitepath)
+                }
+                .alert(isPresented: $hasError1) {
+                    Alert(
+                        title: Text("Error in Hot Sales!"),
+                        message: Text(viewModel.customErrorList["default"]?.localizedDescription ?? ""),
+                        dismissButton: Alert.Button.default(Text("Ok")))
+                }
+                .refreshable {
+                    Task {
+                        await refreshProduct()
+                        await getCategories()
                     }
                 }
+                Spacer()
             }
-            .redacted(reason: loading ? .placeholder: [])
-            .onAppear {
-                UITabBar.showTabBar(animated: true)
-                
-                guard let url = FileManager.default.urls(for: .applicationDirectory, in: .userDomainMask).first
-                else {
-                    return
-                }
-                let sqlitepath = url.appendingPathComponent("TheSoundsStoreCoreData")
-                print(sqlitepath)
-            }
-            .alert(isPresented: $hasError1) {
-                Alert(
-                    title: Text("Error in Hot Sales!"),
-                    message: Text(viewModel.customErrorList["default"]?.localizedDescription ?? ""),
-                    dismissButton: Alert.Button.default(Text("Ok")))
-            }
-            .refreshable {
-                Task {
-                    await refreshProduct()
-                    await getCategories()
-                }
-            }
+            
         }
     }
 }
